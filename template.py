0c40b5a125cfc3eac566b3e20a619066cae30036
Alfred <alfredmountfield@gmail.com>
2019-12-07 20:41:08 +0000

    Added Template

---
+++ template.py
@@ -0,0 +1,13 @@
+"""
+Challenge Description
+"""
+
+import logging
+import sys
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, format='[%(levelname)s] %(message)s', level=logging.INFO)
+    log = logging.getLogger(__name__)
+
+    log.info(f"Part 1: {}")
+    log.info(f"Part 2: {}")
